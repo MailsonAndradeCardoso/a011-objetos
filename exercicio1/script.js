b3b5a8ecbcae73bfb1a5c86b2168d60b6ed6e192
MailsonAndradeCardoso <mailsoncardoso1988@gmail.com>
2022-09-05 15:58:18 -0300

    Exercicios ok

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+//Exercicio 1 - Objetos
+
+//a)
+const sacolao = []
+
+//criando 3 objetos
+let fruta1 = { fruta: "uva", preco: 8.75, disponibilidade: true }
+let fruta2 = { fruta: "mamao", preco: 7.95, disponibilidade: true }
+let fruta3 = {fruta: "pera", preco: 4.59, disponibilidade: false }
+const frutas = ( fruta1, fruta2, fruta3)
+
+//b)
+sacolao.push(fruta1, fruta2, fruta3)
+//c)
+console.log(sacolao)
